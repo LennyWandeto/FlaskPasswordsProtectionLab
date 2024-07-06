4a70e4385e41751e35e93c57dab1ee2e072c1839
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-07-06 21:41:05 +0300

    done

--- server/app.py
+++ server/app.py
@@ -18,26 +18,55 @@ class ClearSession(Resource):
 class Signup(Resource):
     
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username']
-        )
-        user.password_hash = json['password']
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+        
+        if username and password:
+            user = User(username=username)
+            user.password_hash = password
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+
+            return user.to_dict(), 201
+        
+        return {'message': 'Username or password missing'}, 422
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {'message': 'Invalid username or password'}, 401
     pass
 
 class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
     pass
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
